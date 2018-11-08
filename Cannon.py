f667e133ce4febc9e6c8a65bb7fedc9e00a113e1
Kate <markelova.ee@phystech.edu>
2018-11-08 22:59:02 +0300

    started doing the task

---
+++ Cannon.py
@@ -0,0 +1,60 @@
+import math
+
+g = 9.8
+
+
+class Cannon:
+    max_velocity = 10
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.shell_num = None
+        self.direction = math.pi / 4
+
+    def aim(self, x, y):
+
+        pass
+
+    def fire(self, dt):
+
+        pass
+
+
+class Shell:
+    standard_radius = 5
+
+    def __init__(self, x, y, Vx, Vy):
+        self.x, self.y = x, y
+        self.Vx, self.Vy = Vx, Vy
+        self.r = standart_radius
+
+    def go(self, dt):
+        ax, ay = 0, g
+        self.x += self.Vx*dt + ax*(dt**2)/2
+        self.y += self.Vy*dt + ay*(dt**2)/2
+        self.Vx += ax*dt
+        self.Vy += ay*dt
+
+    def detect_collision(self, other):
+        length = ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
+        return length <= self.r + other.r
+
+
+class Target:
+    standard_radius = 5
+
+    def __init__(self, x, y, Vx, Vy):
+        self.x, self.y = x, y
+        self.Vx, self.Vy = Vx, Vy
+        self.r = standard_raduis
+
+    def go(self, dt):
+        ax, ay = 0, g
+        self.x += self.Vx * dt
+        self.y += self.Vy * dt
+        self.Vx += ax * dt
+        self.Vy += ay * dt
+
+    def collide(self, other):
+        pass
