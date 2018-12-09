184b5d1bb80760740d204ba6c7e1a78ebb097431
Kate <markelova.ee@phystech.edu>
2018-12-09 14:07:53 +0300

    finished the second task

---
+++ Tasks/Robot/Robot_task2.py
@@ -0,0 +1,21 @@
+import Robot
+r = Robot.rmap()
+r.lm('task2')
+
+
+def task():
+    for x in range(7):
+        r.up()
+        r.pt()
+        r.up()
+        r.rt()
+        r.pt()
+        r.dn()
+        r.dn()
+        r.pt()
+        r.rt()
+    r.up()
+    r.pt()
+
+
+r.start(task)
