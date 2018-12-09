848490bef43f092acf032e186596e2f096351199
Kate <markelova.ee@phystech.edu>
2018-12-09 14:40:12 +0300

    finished the fifth task

---
+++ Tasks/Robot/Robot_task5.py
@@ -0,0 +1,28 @@
+import Robot
+r = Robot.rmap()
+r.lm('task5')
+
+
+def task():
+    for _ in range(3):
+        for _ in range(3):
+            r.pt()
+            r.dn()
+            r.rt()
+            r.pt()
+            r.rt()
+            r.up()
+            r.pt()
+            r.rt(2)
+        r.pt()
+        r.dn()
+        r.rt()
+        r.pt()
+        r.rt()
+        r.up()
+        r.pt()
+        r.dn(3)
+        r.lt(14)
+
+
+r.start(task)
