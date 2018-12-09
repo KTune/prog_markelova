9e17c028f9ab67a85b5fd03be7e43cebd0f8f570
Kate <markelova.ee@phystech.edu>
2018-12-09 14:19:57 +0300

    finished the fourth task

---
+++ Tasks/Robot/Robot_task4.py
@@ -0,0 +1,20 @@
+import Robot
+r = Robot.rmap()
+r.lm('task4')
+
+
+def task():
+    while r.fl():
+        r.lt()
+    while r.fd():
+        r.dn()
+    for y in range(3):
+        for x in range(5):
+            r.rt()
+            r.up()
+            r.pt()
+        r.lt(5)
+        r.dn(3)
+
+
+r.start(task)
